773048a447adfe534c0a6d3bc3986d43eb981288
iamrashidkhan <109840147+iamrashidkhan@users.noreply.github.com>
2022-07-24 14:43:40 +0530

    Add files via upload

---
+++ areaof.java
@@ -0,0 +1,17 @@
+public class areaof {
+    public static void main(String[] args){
+        int l,b;
+        int sq, rec;
+
+        //Initiazation of length and breadth
+        l = 10;
+        b = 20;
+
+        sq = l*l;
+        rec = l*b;
+
+        System.out.println("Area of square is "+ sq);
+        System.out.println("Area of rectangle is "+ rec);
+    }
+    
+}
